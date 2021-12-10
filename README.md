b8f437766cc9bd395d2d49930f7539886c459b87
Gabriele <gabriele.dinuovo@gmail.com>
2021-12-10 17:49:12 +0100

    New save format and tweaks
    - Changed the saved manga file format to a more decent one, using new lines. I had to add and strip some newlines from the code so bear with the mess.
    - The refresh time is now a variable
    - Messages are sent in Markdown
    - The messages are requested using the telegram offset property, to avoid duplicates and to avoid to have an array of processed messages
    - Using only one quote format...

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ v1 (now private) is not going to be updated anymore since reddit rate limited me
 
 ## Usage:
 - Create a new bot on Telegram. I'm super lazy and I created a very basic one, added to a group with only me and then disabled the `/setjoingroups` option.
-- Add this exact two commands for make it work correctly:  
+- Add this exact three commands for make it work correctly:  
 `addmanga - Adds a new manga to the interested manga list`  
-~~removemanga - Removes a manga from the list of interests~~ Not implemented yet, but maybe you can add it as futureproof?
+`removemanga - Removes a manga from the list of interests`  Finally added!
+`mangalist - List the current active manga alerts`
 - Get the API Token for the bot
 - Obtain the channel ID for the group/chat where you want the bot to send the messages to
 - Create a reddit script from https://www.reddit.com/prefs/apps/ (you must be logged in)
@@ -24,6 +25,6 @@ No messages are sent on Telegram on initialization.
 Use tmux or something similar to avoid to close the python session when you log out from the device
 
 ## Future plans:  
-- [x] move all the auth stuff to a separate file lol
-- [ ] `/removemanga` to remove a manga from the list (maybe using telegram buttons to avoid "guessing" what the user wrote vs what is in the file)
+- [x] ~~move all the auth stuff to a separate file~~ Done!
+- [x] ~~`/removemanga` to remove a manga from the list (maybe using telegram buttons to avoid "guessing" what the user wrote vs what is in the file)~~ Done but without the buttons!
 - [ ] [mangadex.org](https://mangadex.org/) 'followed manga' integration using the API! (it would require to save on the bot files the username and the password tho) ([https://api.mangadex.org/swagger.html#/Feed/get-user-follows-manga-feed](https://api.mangadex.org/swagger.html#/Feed/get-user-follows-manga-feed))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 import configparser
 import os
 from time import sleep
+from datetime import datetime
 
 # DO NOT FILL THE TOKENS HERE
 # Reddit Auth
@@ -13,86 +14,109 @@ REDDIT_BOT_SECRET = ''     # The bot secret token
 TG_BOT_TOKEN = ''
 TG_CHANNEL_ID = ''
 
-# If you have to check a lot of manga or you are getting duplicates, increase those sizes
-LATEST_MANGA_LIST = [''] * 25
-PROCESSED_REQUESTS = [''] * 25
+# If you have to check a lot of manga or you are getting duplicates, increase this number
+MAX_LIST_SIZE = 25
+# Time in minutes between each check
+REFRESH_TIME = 20
 
 # Don't touch! c:
-REQUESTS_INDEX = 0
+LATEST_MANGA_LIST = [''] * MAX_LIST_SIZE
 MANGA_INDEX = 0
+TG_OFFSET_ID = 0
 
 def send_message(message):
-    requests.get(f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage?chat_id={TG_CHANNEL_ID}&text={message}')
+    requests.get(f"https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage?chat_id={TG_CHANNEL_ID}&text={message}&parse_mode=Markdown")
+
+def consolePrint(message):
+    header = "[" + datetime.now().strftime("%d-%m-%Y %H:%M:%S") + "] "
+    print(header + message)
 
 def main():
     try:
-        global MANGA_INDEX, LATEST_MANGA_LIST, PROCESSED_REQUESTS, REQUESTS_INDEX
-        # Reads the titles file
-        fileL = open("lista.txt", "r+")
-        contenutoF = fileL.read()
-        MANGA_LIST = contenutoF.split(';')
-
+        global MANGA_INDEX, LATEST_MANGA_LIST, PROCESSED_REQUESTS, REQUESTS_INDEX, TG_OFFSET_ID
+        MANGA_LIST = []
+        # Read the titles from the file, removing the newlines
+        with open('list.txt', 'r') as listFile:
+            for line in listFile:
+                MANGA_LIST.append(line.replace('\n',''))
+        # Creates the reddit client
         redditC = praw.Reddit(
             client_id = REDDIT_BOT_ID,
             client_secret = REDDIT_BOT_SECRET,
-            user_agent = 'TelegramAlert/0.1'
+            user_agent = 'TelegramAlert/0.4'
         )
+        # The manga subreddit. 
         mangaSubreddit = redditC.subreddit('manga')
         for submission in mangaSubreddit.new(limit=50):
             # I check if it's a release post
             if "[DISC]" in submission.title.upper():
+                # I strip all the strange characters 
                 titleStripped = submission.title.upper().replace("[DISC]", "")
                 titleStripped = re.sub(r'[^A-Za-z0-9 ]+', '', titleStripped)
                 titleStripped = titleStripped.lower()
-                if any(titolo in titleStripped for titolo in MANGA_LIST):
+                if any(titleStripped in savedTitle for savedTitle in MANGA_LIST):
                     # I check if I haven't already checked a post with the same ID
                     if submission.id not in LATEST_MANGA_LIST:
-                        print("Alert for " + submission.title)
-                        # creo e mando il messaggio
-                        urlCompleto = "https://www.reddit.com" + submission.permalink
-                        messaggio = 'New manga chapter: ' + submission.title + '\n' + urlCompleto
-                        send_message(messaggio)
+                        consolePrint("Alert for " + submission.title)
+                        # I create and send the message
+                        completeUrl = 'https://www.reddit.com' + submission.permalink
+                        message = "New manga chapter: " + submission.title + "\n" + completeUrl
+                        send_message(message)
                         # I save the ID to avoid duplicates
                         LATEST_MANGA_LIST[MANGA_INDEX] = submission.id
                         MANGA_INDEX += 1
-                        if(MANGA_INDEX > 24):
-                            print("Reset manga index")
+                        if(MANGA_INDEX > (MAX_LIST_SIZE - 1)):
+                            consolePrint("Reset manga index")
                             MANGA_INDEX = 0
 
         # I read the received messages
-        messaggiRaw = requests.get(f'https://api.telegram.org/bot{TG_BOT_TOKEN}/getUpdates?chat_id={TG_CHANNEL_ID}')
+        messaggiRaw = requests.get(f"https://api.telegram.org/bot{TG_BOT_TOKEN}/getUpdates?chat_id={TG_CHANNEL_ID}&offset={TG_OFFSET_ID}")
         messaggiDecoded = messaggiRaw.json()
-        for messaggio in messaggiDecoded['result']:
-            # I check that the message is from the correct chat and that I haven't already processed it
-            if messaggio['update_id'] not in PROCESSED_REQUESTS:
-                idCanale = str(messaggio['message']['chat']['id'])
-                if idCanale == TG_CHANNEL_ID:
-                    if messaggio['message']['text'].startswith('/addmanga'):
-                        # If it's a correct command, I extract the manga name and I add it to the list
-                        posSpazio = messaggio['message']['text'].find(' ')
-                        nomeManga = messaggio['message']['text'][posSpazio:].lower().strip()
-                        if len(nomeManga) > 0 and posSpazio != -1:
-                            publicMsg = "Aggiungo " + nomeManga
-                            toWrite = ";" + nomeManga
-                            fileL.write(toWrite)
-                            PROCESSED_REQUESTS[REQUESTS_INDEX] = messaggio['update_id']
-                            REQUESTS_INDEX += 1
-                            print(publicMsg)
-                            send_message(publicMsg)
-                            if(REQUESTS_INDEX > 24):
-                                print("Reset requests index")
-                                REQUESTS_INDEX = 0
-
+        for message in messaggiDecoded['result']:
+            # I save the offset id to avoid requesting the same message twice
+            TG_OFFSET_ID = message['update_id'] + 1
+            if message['message']['text'].startswith('/addmanga'):
+                # If it's a correct command, I extract the manga name and I add it to the file
+                spacePosition = message['message']['text'].find(' ')
+                mangaName = message['message']['text'][spacePosition:].lower().strip()
+                if len(mangaName) > 0 and spacePosition != -1:
+                    # Writing in the file
+                    with open('list.txt', 'a') as listFile:
+                        listFile.write(mangaName)
+                        listFile.write("\n")
+                    # I reload the manga_list variable to include the newest addition
+                    MANGA_LIST = []
+                    with open('list.txt', 'r') as listFile:
+                        for line in listFile:
+                            MANGA_LIST.append(line.replace('\n',''))
+                    publicMsg = "Added the manga `" + mangaName + "` to the alert list."
+                    consolePrint(publicMsg)
+                    send_message(publicMsg)
+                    
     except Exception as e:
-        print('There was an exception:\n' + str(e))
+        print("There was an exception:\n" + str(e))
         send_message("The bot crashed, please check the console for more informations about the error.")
+        exit(1)
     finally:
-        fileL.close()
-        print('Ending now')
+        consolePrint('Ending now')
 
 if __name__ == "__main__":
+    # Check for the old list file and converts it to the newer list file
+    if os.path.isfile('lista.txt'):
+        print("Converting the old list file to a newer, v2, format.")
+        os.rename('lista.txt','oldList.txt')
+        oldMangaList = ""
+        with open('oldList.txt', 'r') as oldMangaListFile:
+            oldMangaList = oldMangaListFile.read()
+        oldMangaListArray = oldMangaList.split(';')
+        with open('list.txt','w') as newList:
+            for manga in oldMangaListArray:
+                if len(manga.strip()) > 0:
+                    newList.write(manga)
+                    newList.write("\n")
+    
     # Check for config file and loads it
-    if os.path.isfile("auth.ini"):
+    if os.path.isfile('auth.ini'):
         config = configparser.ConfigParser()
         config.read('auth.ini')
         TG_BOT_TOKEN = config['Telegram']['TG_BOT_TOKEN']
@@ -102,14 +126,13 @@ if __name__ == "__main__":
         if TG_BOT_TOKEN == "" or TG_CHANNEL_ID == "" or REDDIT_BOT_ID == "" or REDDIT_BOT_SECRET == "":
             print("Please fill all the required informations! Only the Mangadex part is optional!")
             exit(1)
-        print("Program initialized, starting the main loop.")
+        consolePrint("Program initialized, starting the main loop.")
         # The "main" loop
         while(True):
             main()
-            print('Sleeping for 20 mins')
-            sleep(20 * 60)
+            consolePrint(f"Sleeping for {REFRESH_TIME} mins")
+            sleep(REFRESH_TIME * 60)
     else:
         print("Error: the file auth.ini wasn't found. Please insert all the required fields and rerun the program.")
-        f = open("auth.ini", "w")
-        f.write("[Telegram]\nTG_BOT_TOKEN = \nTG_CHANNEL_ID = \n\n[Reddit]\nREDDIT_BOT_ID = \nREDDIT_BOT_SECRET = \n\n[Mangadex]\nMD_USERNAME = \nMD_PASSWORD = \n")
-        f.close()
+        with open('auth.ini', 'w') as f:
+            f.write("[Telegram]\nTG_BOT_TOKEN = \nTG_CHANNEL_ID = \n\n[Reddit]\nREDDIT_BOT_ID = \nREDDIT_BOT_SECRET = \n\n[Mangadex]\nMD_USERNAME = \nMD_PASSWORD = \n")
